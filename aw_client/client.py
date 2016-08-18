67d6c83ad255c9b39049b529a5a0bfbb62538f9e
johan-bjareholt <johanthepro@gmail.com>
2016-08-18 14:00:16 +0200

    Queue and create_bucket now works

--- aw_client/client.py
+++ aw_client/client.py
@@ -23,7 +23,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 # TODO: Should probably use OAuth or something
 
 class ActivityWatchClient:
-    def __init__(self, client_name: str, event_type: str, testing=False):
+    def __init__(self, client_name: str, testing=False):
         self.logger = logging.getLogger("aw-client")
         self.testing = testing
 
@@ -37,7 +37,7 @@ class ActivityWatchClient:
 
         # Setup failed queues dir
         self.data_dir = appdirs.user_data_dir("aw-client")
-        self.failed_queues_dir = "{directory}/failed_events".format(directory=data_dir)
+        self.failed_queues_dir = "{}/failed_events/{}/".format(self.data_dir, self.client_name)
         if not os.path.exists(self.failed_queues_dir):
             os.makedirs(self.failed_queues_dir)
         
@@ -62,7 +62,7 @@ class ActivityWatchClient:
     def _start_session(self):
         session_id = "{}#{}".format(self.client_name, int(time.time() * 1000))
         try:
-            resp = self._send("session/start", {"session_id": session_id})
+            resp = self._post("session/start", {"session_id": session_id})
             data = resp.json()
             self.session = {"session_id": session_id, "session_key": data["session_key"]}
         except req.RequestException as e:
@@ -70,31 +70,29 @@ class ActivityWatchClient:
 
     def _stop_session(self):
         assert self.session_active
-        resp = self._send("session/stop", self.session)
+        resp = self._post("session/stop", self.session)
         if resp:
             self.session = {}
 
     def _queue_failed_event(self, bucket: str, data: dict):
         # Find failed queue file
         endpoint = 'buckets/{}/events'.format(bucket)
-        self.queue_file = "{d}/{f}".format(d=self.failed_queues_dir, f=self.client_name)
-        with open(self.queue_file, "a+") as queue_fp:
+        queue_file = "{d}/{f}".format(d=self.failed_queues_dir, f=bucket)
+        with open(queue_file, "a+") as queue_fp:
             queue_fp.write(json.dumps(data)+"\n")
 
-    def _send_failed_events(self):
-        # TODO: Verify that it still works
-        if os.path.exists(self.queue_file):
-            failed_events = []
-            for queue_file in os.listdir(self.failed_queues_dir):
-                with open(queue_file, "r") as queue_fp:
-                    queue_fp.seek(0, 0)
-                    for event in queue_fp:
-                        failed_events.append(Event(**json.loads(event)))
-                    logging.info("Sent failed events: {}".format(failed_events))
-                open(self.queue_file, "w").close() # Truncate file
-                self.send_events(failed_events)
+    def _post_failed_events(self):
+        failed_events = []
+        for bucket in os.listdir(self.failed_queues_dir):
+            queue_file_path = "{}/{}".format(self.failed_queues_dir, bucket)
+            with open(queue_file_path, "r") as queue_fp:
+                for event in queue_fp:
+                    failed_events.append(Event(**json.loads(event)))
+                self.logger.info("Sent failed events: {}".format(failed_events))
+            open(queue_file_path, "w").close() # Truncate file
+            self.send_events(bucket, failed_events)
 
-    def _send(self, endpoint: str, data: dict) -> Optional[req.Response]:
+    def _post(self, endpoint: str, data: dict) -> Optional[req.Response]:
         headers = {"Content-type": "application/json"}
         # FIXME: Use HTTPS whenever possible!
         url = "http://{}:{}/api/0/{}".format(self.server_hostname, self.server_port, endpoint)
@@ -102,12 +100,18 @@ class ActivityWatchClient:
         response.raise_for_status()
         return response
 
+    def _get(self, endpoint: str) -> Optional[req.Response]:
+        url = "http://{}:{}/api/0/{}".format(self.server_hostname, self.server_port, endpoint)
+        response = req.get(url)
+        response.raise_for_status()
+        return response
+    
     def send_event(self, bucket, event: Union[Event, List[Event]]):
         # TODO: Notice if server responds with invalid session and create a new one
         endpoint = "buckets/{}/events".format(bucket)
         data = event.to_json_dict()
         try:
-            self._send(endpoint, data)
+            self._post(endpoint, data)
             self.logger.debug("Sent event to server: {}".format(event))
         except req.RequestException as e:
             self.logger.warning("Failed to send event to server ({})".format(e))
@@ -118,28 +122,30 @@ class ActivityWatchClient:
         endpoint = "buckets/{}/events".format(bucket)
         data = [event.to_json_dict() for event in events]
         try:
-            self._send(endpoint, data)
+            self._post(endpoint, data)
             self.logger.debug("Sent events to server: {}".format(events))
         except req.RequestException as e:
             self.logger.warning("Failed to send events to server ({})".format(e))
             for event in data:
                 self._queue_failed_event(bucket, event)
 
-    
-    def create_bucket(bucket_id: str, event_type: str):
+    def get_buckets(self):
+        return self._get('buckets')
+
+    def create_bucket(self, bucket_id: str, event_type: str):
         # Check if bucket exists
-        endpoint = "buckets/{}/create".format(bucket_id)
+        endpoint = "buckets/{}".format(bucket_id)
         data = {
             'client': self.client_name,
             'hostname': self.client_hostname,
             'type': event_type,
         }
         try:
-            self._send(endpoint, data)
+            self._post(endpoint, data)
         except req.RequestException as e:
-            logging.error("Unable to create bucket")
+            self.logger.error("Unable to create bucket: {}".format(e))
 
-        
+
 
 class QueueTimerThread(threading.Thread):
     def __init__(self, aw_client):
@@ -149,6 +155,6 @@ class QueueTimerThread(threading.Thread):
 
     def run(self):
         while True:
-            self.aw_client._send_failed_events()
+            self.aw_client._post_failed_events()
             time.sleep(180)
 
