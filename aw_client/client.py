71055f5c74a8e89913782414cd6200bb1826eed3
Johan Bjäreholt <johan@bjareho.lt>
2017-01-11 15:19:22 +0100

    Requestqueue and refactoring - The failed event queue is now a failed request queue, adding support for replace_last_event - The failed request queue fixes issues when wrong events are being replaced - Replaced create_bucket with setup_bucket and added a connect method which improves the API a lot - Refactoring here and there

--- aw_client/client.py
+++ aw_client/client.py
@@ -14,15 +14,17 @@ from . import config
 
 # FIXME: This line is probably badly placed
 logging.getLogger("requests").setLevel(logging.WARNING)
+logger = logging.getLogger("aw.client")
 
 
 # TODO: Should probably use OAuth or something
 
 class ActivityWatchClient:
     def __init__(self, client_name: str, testing=False):
-        self.logger = logging.getLogger("aw.client")
         self.testing = testing
 
+        self.connected = False
+        self.buckets = []
         self.session = {}
 
         self.client_name = client_name + ("-testing" if testing else "")
@@ -33,31 +35,16 @@ class ActivityWatchClient:
 
         # Setup failed queues dir
         self.data_dir = appdirs.user_data_dir("aw-client")
-        self.failed_queues_dir = os.path.join(self.data_dir, "failed_events", self.client_name)
+        self.failed_queues_dir = os.path.join(self.data_dir, "failed_events")
         if not os.path.exists(self.failed_queues_dir):
             os.makedirs(self.failed_queues_dir)
+        self.queue_file = os.path.join(self.failed_queues_dir, self.client_name)
 
-        # Send old failed events
-        self._post_failed_events()
-        QueueTimerThread(self).start()
+        self.reconnect_thread = ReconnectThread(self)
 
-    def _queue_failed_event(self, bucket: str, data: dict):
-        # Find failed queue file
-        queue_file = os.path.join(self.failed_queues_dir, bucket)
-        with open(queue_file, "a+") as queue_fp:
-            queue_fp.write(json.dumps(data) + "\n")
-
-    def _post_failed_events(self):
-        failed_events = []
-        for bucket in os.listdir(self.failed_queues_dir):
-            queue_file_path = os.path.join(self.failed_queues_dir, bucket)
-            with open(queue_file_path, "r") as queue_fp:
-                for event in queue_fp:
-                    failed_events.append(Event(**json.loads(event)))
-                if len(failed_events) != 0:
-                    self.logger.info("Sent failed events: {}".format(failed_events))
-                    open(queue_file_path, "w").close()  # Clear file
-                    self.send_events(bucket, failed_events)
+    #
+    #   Get/Post base requests
+    #
 
     def _post(self, endpoint: str, data: dict) -> Optional[req.Response]:
         headers = {"Content-type": "application/json"}
@@ -72,72 +59,150 @@ class ActivityWatchClient:
         response.raise_for_status()
         return response
 
-    def send_event(self, bucket, event: Event):
-        endpoint = "buckets/{}/events".format(bucket)
-        data = event.to_json_dict()
-        try:
-            self._post(endpoint, data)
-            self.logger.debug("Sent event to server: {}".format(event))
-        except req.RequestException as e:
-            self.logger.warning("Failed to send event to server ({})".format(e))
-            self._queue_failed_event(bucket, data)
-
-    def send_events(self, bucket, events: List[Event]):
-        endpoint = "buckets/{}/events".format(bucket)
-        data = [event.to_json_dict() for event in events]
-        try:
-            self._post(endpoint, data)
-            self.logger.debug("Sent events to server: {}".format(events))
-        except req.RequestException as e:
-            self.logger.warning("Failed to send events to server ({})".format(e))
-            for event in data:
-                self._queue_failed_event(bucket, event)
-
-    def replace_last_event(self, bucket, event: Event):
-        endpoint = "buckets/{}/events/replace_last".format(bucket)
-        data = event.to_json_dict()
-        try:
-            self._post(endpoint, data)
-            self.logger.debug("Sent event to server: {}".format(event))
-        except req.RequestException as e:
-            self.logger.warning("Failed to send event to server ({})".format(e))
-            self._queue_failed_event(bucket, data)
-
-    def get_buckets(self):
-        return self._get('buckets').json()
+    #
+    #   Event get/post requests
+    #
 
     def get_events(self, bucket) -> List[Event]:
         endpoint = "buckets/{}/events".format(bucket)
         events = self._get(endpoint).json()
         return [Event(**event) for event in events]
 
-    def create_bucket(self, bucket_id, event_type: str) -> bool:
-        # Check if bucket exists
-        buckets = self.get_buckets()
-        if bucket_id in buckets:
-            return False  # Don't do anything if bucket already exists
-        else:
-            # Create bucket
-            endpoint = "buckets/{}".format(bucket_id)
-            data = {
-                'client': self.client_name,
-                'hostname': self.client_hostname,
-                'type': event_type,
-            }
+    def send_event(self, bucket, event: Event):
+        endpoint = "buckets/{}/events".format(bucket)
+        data = event.to_json_dict()
+        if self.connected:
             try:
                 self._post(endpoint, data)
+                logger.debug("Sent event to server: {}".format(event))
             except req.RequestException as e:
-                self.logger.error("Unable to create bucket: {}".format(e))
+                self._connection_lost()
+                logger.warning("Failed to send event to server ({})".format(e))
+                self._queue_failed_request(endpoint, data)
+        else:
+            self._queue_failed_request(endpoint, data)
+
+    def send_events(self, bucket, events: List[Event], ignore_failed=False):
+        endpoint = "buckets/{}/events".format(bucket)
+        data = [event.to_json_dict() for event in events]
+        if self.connected:
+            try:
+                self._post(endpoint, data)
+                logger.debug("Sent events to server: {}".format(events))
+            except req.RequestException as e:
+                self._connection_lost()
+                logger.warning("Failed to send events to server ({})".format(e))
+                for event in data:
+                    self._queue_failed_request(endpoint, event)
+        else:
+            self._queue_failed_request(endpoint, data)
+
+    def replace_last_event(self, bucket, event: Event):
+        endpoint = "buckets/{}/events/replace_last".format(bucket)
+        data = event.to_json_dict()
+        if self.connected:
+            try:
+                self._post(endpoint, data)
+                logger.debug("Sent event to server: {}".format(event))
+            except req.RequestException as e:
+                self._connection_lost()
+                logger.warning("Failed to send event to server ({})".format(e))
+                self._queue_failed_request(endpoint, data)
+        else:
+            self._queue_failed_request(endpoint, data)
+
+    #
+    #   Bucket get/post requests
+    #
+
+    def get_buckets(self):
+        return self._get('buckets').json()
+
+    def setup_bucket(self, bucket_id, event_type: str) -> bool:
+        self.buckets.append({"bid": bucket_id, "etype": event_type})
+        if self.connected:
+            self._create_buckets()
+
+    def _create_buckets(self):
+        # Check if bucket exists
+        buckets = self.get_buckets()
+        for bucket in self.buckets:
+            if bucket['bid'] in buckets:
+                return False  # Don't do anything if bucket already exists
+            else:
+                # Create bucket
+                endpoint = "buckets/{}".format(bucket['bid'])
+                data = {
+                    'client': self.client_name,
+                    'hostname': self.client_hostname,
+                    'type': bucket['etype'],
+                }
+                try:
+                    self._post(endpoint, data)
+                except req.RequestException as e:
+                    logger.error("Failed to create bucket: {}".format(e))
+                return True
+
+    #
+    #   Failed request queue handling
+    #
+
+    def _queue_failed_request(self, endpoint: str, data: dict):
+        # Find failed queue file
+        entry = {"endpoint": endpoint, "data": data}
+        with open(self.queue_file, "a+") as queue_fp:
+            queue_fp.write(json.dumps(entry) + "\n")
+
+    def _post_failed_requests(self):
+        failed_requests = []
+        for bucket in os.listdir(self.failed_queues_dir):
+            with open(self.queue_file, "r") as queue_fp:
+                for request in queue_fp:
+                    failed_requests.append(json.loads(request))
+                if len(failed_requests) != 0:
+                    open(self.queue_file, "w").close()  # Clear file
+                    logger.info("Sending {} failed events: {}".format(len(failed_requests), failed_requests))
+                    for request in failed_requests:
+                        self._post(request['endpoint'], request['data'])
+
+    #
+    #   Connection methods
+    #
+
+    def connect(self):
+        if self._try_connect():
+            self.connected = True
+        else:
+            logger.warning("Can't connect to aw-server, will queue events until connection is available")
+            if not self.reconnect_thread.is_alive():
+                self.reconnect_thread.start()
+
+    def _try_connect(self):
+        try:
+            self._create_buckets()
+            self._post_failed_requests()
             return True
+        except req.RequestException as e:
+            return False
+
+    def _connection_lost(self):
+        """
+            Call this when we lose connection to the server
+        """
+        self.connected = False
+        if not self.reconnect_thread.is_alive():
+            self.reconnect_thread.start()
 
 
-class QueueTimerThread(threading.Thread):
+class ReconnectThread(threading.Thread):
     def __init__(self, client):
         threading.Thread.__init__(self)
         self.daemon = True
         self.client = client
 
     def run(self):
-        while True:
-            time.sleep(180)
-            self.client._post_failed_events()
+        while not self.client.connected:
+            if self.client._try_connect():
+                logger.warning("Connection to aw-server established again")
+            else:
+                time.sleep(60)
