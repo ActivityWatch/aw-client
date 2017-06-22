7c1c4f22774e955ead8340eae5cd2716ad95b6da
Johan Bjäreholt <johan@bjareho.lt>
2017-06-22 15:49:16 +0200

    Updated test_client.py so it works with newer aw-server versions

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,7 +8,7 @@ from aw_core.models import Event
 from aw_client import ActivityWatchClient
 
 def create_unique_event():
-    return Event(label=str(random()), timestamp=datetime.now(timezone.utc), duration=timedelta())
+    return Event(data={"label": str(random())}, timestamp=datetime.now(timezone.utc), duration=timedelta())
 
 client = ActivityWatchClient("aw-test-client", testing=True)
 
@@ -16,21 +16,22 @@ bucket_name = "test-bucket"
 bucket_etype = "test"
 client.setup_bucket(bucket_name, bucket_etype)
 
-e1 = create_unique_event()
-e2 = create_unique_event()
-e3 = create_unique_event()
-client.send_event(bucket_name, e1)
-client.replace_last_event(bucket_name, e2)
-client.replace_last_event(bucket_name, e3)
-
 client.connect()
 time.sleep(1)
 
+e1 = create_unique_event()
+e2 = create_unique_event()
+e3 = create_unique_event()
+events = [e1, e2, e3]
+client.send_events(bucket_name, events)
+
 print("Getting events")
-events = client.get_events(bucket_name)
+fetched_events = client.get_events(bucket_name, limit=len(events))
 
 print("Asserting events")
-assert events[0]['label'] == e3['label']
+print(fetched_events)
+print(events)
+assert fetched_events == events
 
 print("Getting bucket")
 buckets = client.get_buckets()
@@ -38,3 +39,5 @@ print("Asserting bucket")
 assert bucket_name in buckets
 assert bucket_name == buckets[bucket_name]['id']
 assert bucket_etype == buckets[bucket_name]['type']
+
+client.delete_bucket(bucket_name)
