5791519bc6174d80d534979b1da7b751871cfe5f
Erik Bjäreholt <erik@bjareho.lt>
2018-11-03 15:27:56 +0100

    removed direct URL requirement from setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(name='aw-client',
       packages=['aw_client'],
       install_requires=[
           # for whatever reason, pip doesn't resolve dependencies in requirements.txt when package is installed by a dependent
+          # but unfortunately, pipenv won't allow direct URL requirements: https://travis-ci.org/ActivityWatch/aw-research/jobs/450184764
+          #'aw-core @ git+https://github.com/ActivityWatch/aw-core.git#egg=aw-core',
           'aw-core',
           'requests',
           'persist-queue'
